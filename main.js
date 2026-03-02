7935b5e7b7d651789bf38b91c12b0274e1efa239
Kayden <62318961+Marvoos@users.noreply.github.com>
2026-03-01 20:28:32 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
-const ctx = document.getElementById("mycanvas");
-
 // Beginning of Jumbotron button programming
 
 const jumboDescTxt = document.getElementById("jumbo-desc");
 const jumboRoundBtnDiv = document.querySelector(".round-btn-div");
 let roundBtnSelected = 0;
 const currYear = "Fourth year";
-const currUniversity = ["https://www.uwindsor.ca/", "The University of Windsor"];
-const currProgram = ["http://future.uwindsor.ca/program/computer-science/", "Computer Science"]
+const currUniversity = ["https://www.uwindsor.ca/", "the University of Windsor"];
+const currProgram = ["https://www.future.uwindsor.ca/program/computer-science-general/", "Computer Science"];
 const ageOfMe = parseInt(((new Date()) - (new Date("2004-11-24"))) / 3.154e+10);
 const jumboBodyText = [
-    `<p>Hi! My name is Kayden, I'm going through my <span class="highlight-txt">${currYear}</span> at <span class="highlight-txt"><a href="${currUniversity[0]}" target="_blank">${currUniversity[1]}</a></span> for <span class="highlight-txt"><a href="${currProgram[0]}" target="_blank">${currProgram[1]}</a></span>.</p>`,
+    `<p>I'm Kayden! Of course, you already know that. So let's get into it...</p>`,
     `<p>At <span class="highlight-txt">${ageOfMe} years old</span> I am an aspiring <span class="highlight-txt">programmer</span> and <span class="highlight-txt">creative</span> dedicated to bringing an <span class="highlight-txt">imaginative mind</span> to the field.</p>`,
-    `<p>On this page, I have given you, the <span class="highlight-txt important-txt">reader</span>, a chance to glance at all of my projects, my resume, and developmental experiences.</p>`
+    `<p>I am a <span class="highlight-txt">lifelong learner</span> who will attempt to learn anything when given the right resources and time.</p>`,
+    `<p>On this page, I have given you the chance to view my projects, my resume, and my skills development experience.</p>`
 ];
 
 function changeTextIndex(newIndex) {
@@ -73,15 +72,6 @@ const getMonthYearDate = (startDateInMs) => {
 }
 
 const jobs = [
-    {
-        jobTitle: "Kennel Attendent",
-        companyName: "Blenheim Veterinary Hospital",
-        startAndEndDates: `${getMonthYearDate(new Date("2018-11-02"))} - ${getMonthYearDate(new Date("2022-08-02"))}`,
-        timeInPos: getDateInPosition(new Date("2018-11-02"), new Date("2022-08-02")),
-        jobDesc: "Nightly clean of the veterinary hospital. This includes taking out the garbage, washing dishes, cleaning kennels and cages, disinfecting surfaces, vacuuming, mopping, and ensuring the building is locked up and clean for the next day.",
-        skillsDeveloped: ["Time Management","Organization","Cleaning"]
-    },
-
     {   
         jobTitle: "Clerical IT",
         companyName: "Lambton Kent District School Board",
@@ -107,8 +97,123 @@ const jobs = [
         timeInPos: getDateInPosition(new Date("2023-09-02"), new Date("2025-01-02")),
         jobDesc: "The job of a Teaching assistant at the University of Windsor is to assist an assigned professor with teaching a specific subject. In this case, in COMP2120 'Object Oriented Programming', assisting the professor means hosting labs for students, proctoring for exams, answering emails and holding weekly office hours, and marking exams, labs, and assignments.",
         skillsDeveloped: ["Teaching", "Furthering Programming Skills", "Communication Skills", "Professionalism", "Time Management"]
-    }
+    },
+
+    {
+        jobTitle: "Kennel Attendent",
+        companyName: "Blenheim Veterinary Hospital",
+        startAndEndDates: `${getMonthYearDate(new Date("2018-11-02"))} - ${getMonthYearDate(new Date("2022-08-02"))}`,
+        timeInPos: getDateInPosition(new Date("2018-11-02"), new Date("2022-08-02")),
+        jobDesc: "Nightly clean of the veterinary hospital. This includes taking out the garbage, washing dishes, cleaning kennels and cages, disinfecting surfaces, vacuuming, mopping, and ensuring the building is locked up and clean for the next day.",
+        skillsDeveloped: ["Time Management","Organization","Cleaning"]
+    },
 ]
 
 // End of jobs section
 
+const canvas = document.getElementById("mycanvas");
+const ctx = canvas.getContext("2d");
+
+// End of canvas section
+
+// Beginning of experiences section
+const courseContainer = document.getElementById("courses");
+
+
+const courses = [
+    {
+        commonName: "Computer Networks",
+        courseCode: "COMP3670",
+        department: "Computer Science",
+        description: "",
+        learningGoals: "",
+        projects: ""
+    },
+    {
+        commonName: "World Wide Web Information Systems Development",
+        courseCode: "COMP3340",
+        department: "Computer Science",
+        description: "",
+        learningGoals: "",
+        projects: ""
+    },
+    {
+        commonName: "Advanced Website Design",
+        courseCode: "COMP2707",
+        department: "Computer Science",
+        description: "",
+        learningGoals: "",
+        projects: ""
+    },
+    {
+        commonName: "Web Based Data Management",
+        courseCode: "COMP3077",
+        department: "Computer Science",
+        description: "",
+        learningGoals: "",
+        projects: ""
+    },
+    {
+        commonName: "Database Management Systems",
+        courseCode: "COMP3150",
+        department: "Computer Science",
+        description: "",
+        learningGoals: "",
+        projects: ""
+
+    },
+    {
+        commonName: "Intro to Multimedia Systems",
+        courseCode: "COMP3500",
+        department: "Computer Science",
+        description: "",
+        learningGoals: "",
+        projects: ""
+    },
+    {
+        commonName: "Intro to Computer Graphics",
+        courseCode: "COMP3520",
+        department: "Computer Science",
+        description: "",
+        learningGoals: "",
+        projects: ""
+    },
+    {
+        commonName: "Language, Grammar Translators",
+        courseCode: "COMP2140",
+        department: "Computer Science",
+        description: "",
+        learningGoals: "",
+        projects: ""
+    },
+];
+
+const columnClasses = ["third-col", "fourth-col"];
+
+const randomColClasses = () => {
+    const randomVal = Math.floor(Math.random() * columnClasses.length);
+    return columnClasses[randomVal];
+}
+
+courses.forEach((course) => {
+    const courseName = course.commonName;
+    const department = course.department;
+    //const description = course.description;
+
+    const courseCardDiv = document.createElement("div");
+    const courseNameh3 = document.createElement("h3");
+    const departmentP = document.createElement("p");
+    const chevron = 1;
+
+    courseCardDiv.classList.add("course-card", randomColClasses());
+
+
+    courseNameh3.innerText = courseName;
+    departmentP.innerText = department;
+
+    courseCardDiv.appendChild(courseNameh3);
+    courseCardDiv.appendChild(departmentP);
+    courseContainer.appendChild(courseCardDiv);
+
+
+})
